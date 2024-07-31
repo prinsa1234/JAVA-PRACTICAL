b9981e1498c60a974ed30bb54f4bb8b87acbf68f
Prinsa Gevariya <147602461+prinsa1234@users.noreply.github.com>
2024-07-31 13:48:37 +0530

    Create practical12.java

---
+++ part-3/practical12.java
@@ -0,0 +1,10 @@
+public class practical12{
+	public static void main(String args[]){
+		System.out.println("In rupees:" +args[0]);
+		int a =Integer.parseInt(args[0]);
+
+		
+		int b=a*100;
+		System.out.println("In pound:"+ b);
+}
+}
