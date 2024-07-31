258b5c0f432972d48ac19bc9e1746b17c7d53b0e
Prinsa Gevariya <147602461+prinsa1234@users.noreply.github.com>
2024-07-31 13:46:52 +0530

    Create prac11.java

---
+++ part-2/prac11.java
@@ -0,0 +1,11 @@
+import java.lang.String;
+public class prac11 {
+    public static void main(String[] args) {
+       String str="CHARUSAT UNIVERSITY"; 
+       System.out.println(str);
+       String s=str;
+       System.out.println("string in lowe case: "+s.toLowerCase());
+       System.out.println("string length is :"+s.length());
+       System.out.println("string replace in character: "+s.replace("H", "P"));
+    }
+}
