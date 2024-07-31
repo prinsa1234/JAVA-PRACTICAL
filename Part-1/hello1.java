44d502823b8e63347f0de6b36fb19359df76d2f6
Prinsa Gevariya <147602461+prinsa1234@users.noreply.github.com>
2024-07-31 14:30:29 +0530

    Create hello1.java

---
+++ Part-1/hello1.java
@@ -0,0 +1,22 @@
+class hello1
+{ static 
+	{
+		
+		System.out.println("hiii..");
+	}
+	//instance
+	{
+	System.out.println("hdguwiyweo");
+	}
+ public static void main(String args[])
+ { 
+    hello h=new hello(); //for calling a instance...
+    System.out.println("hello");
+	
+ }
+ static{
+	 System.out.println("hfuweirw");
+	
+ }
+ 
+}
